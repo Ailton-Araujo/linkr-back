a2a43ea0d8f4762aca8e2f7dd11accc4272da1e3
Erico Bruner <ericodasilvabruner@gmail.com>
2023-08-15 21:13:21 -0300

    Update postsRouter.js

--- src/routes/postsRouter.js
+++ src/routes/postsRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import validateAuth from "../middlewares/validateAuth";
+import validateAuth from "../middlewares/validateAuth.js";
 
 const postsRouter = Router();
 
