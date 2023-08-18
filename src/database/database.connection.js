366e42df7dded4eb55ab0784d2342a39f7c5c1b8
Erico Bruner <ericodasilvabruner@gmail.com>
2023-08-18 00:13:30 -0300

    Update database.connection.js

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -6,9 +6,6 @@ const { Pool } = pg;
 
 const configDatabase = {
   connectionString: process.env.DATABASE_URL,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-  ssl: false,
 };
 
 if (process.env.NODE_ENV === "production") configDatabase.ssl = true;
