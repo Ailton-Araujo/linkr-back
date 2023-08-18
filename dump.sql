ad35ab307c16b1f22c0cdf2bbc49180abea9f966
Ailton Araujo <ailton.correia.araujo@gmail.com>
2023-08-18 19:07:15 -0300

    build:dumbDB

---
+++ dump.sql
@@ -0,0 +1,363 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 15.3
+-- Dumped by pg_dump version 15.3
+
+-- Started on 2023-08-18 18:55:41
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 5 (class 2615 OID 2200)
+-- Name: public; Type: SCHEMA; Schema: -; Owner: -
+--
+
+-- *not* creating schema, since initdb creates it
+
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 223 (class 1259 OID 16580)
+-- Name: hashPost; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public."hashPost" (
+    id integer NOT NULL,
+    "hashtagId" integer NOT NULL,
+    "postId" integer NOT NULL
+);
+
+
+--
+-- TOC entry 222 (class 1259 OID 16579)
+-- Name: hashPost_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public."hashPost_id_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- TOC entry 3183 (class 0 OID 0)
+-- Dependencies: 222
+-- Name: hashPost_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public."hashPost_id_seq" OWNED BY public."hashPost".id;
+
+
+--
+-- TOC entry 217 (class 1259 OID 16490)
+-- Name: hashtags; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.hashtags (
+    id integer NOT NULL,
+    hashtag character varying NOT NULL
+);
+
+
+--
+-- TOC entry 216 (class 1259 OID 16489)
+-- Name: hashtags_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.hashtags_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- TOC entry 3184 (class 0 OID 0)
+-- Dependencies: 216
+-- Name: hashtags_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.hashtags_id_seq OWNED BY public.hashtags.id;
+
+
+--
+-- TOC entry 221 (class 1259 OID 16573)
+-- Name: likes; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.likes (
+    id integer NOT NULL,
+    "userId" integer NOT NULL,
+    "postId" integer NOT NULL
+);
+
+
+--
+-- TOC entry 220 (class 1259 OID 16572)
+-- Name: likes_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.likes_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- TOC entry 3185 (class 0 OID 0)
+-- Dependencies: 220
+-- Name: likes_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.likes_id_seq OWNED BY public.likes.id;
+
+
+--
+-- TOC entry 219 (class 1259 OID 16564)
+-- Name: posts; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.posts (
+    id integer NOT NULL,
+    "userId" integer NOT NULL,
+    link text NOT NULL,
+    description text NOT NULL
+);
+
+
+--
+-- TOC entry 218 (class 1259 OID 16563)
+-- Name: posts_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.posts_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- TOC entry 3186 (class 0 OID 0)
+-- Dependencies: 218
+-- Name: posts_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.posts_id_seq OWNED BY public.posts.id;
+
+
+--
+-- TOC entry 215 (class 1259 OID 16470)
+-- Name: users; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    email text NOT NULL,
+    password text NOT NULL,
+    username text NOT NULL,
+    image text NOT NULL
+);
+
+
+--
+-- TOC entry 214 (class 1259 OID 16469)
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- TOC entry 3187 (class 0 OID 0)
+-- Dependencies: 214
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- TOC entry 3016 (class 2604 OID 16583)
+-- Name: hashPost id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public."hashPost" ALTER COLUMN id SET DEFAULT nextval('public."hashPost_id_seq"'::regclass);
+
+
+--
+-- TOC entry 3013 (class 2604 OID 16493)
+-- Name: hashtags id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.hashtags ALTER COLUMN id SET DEFAULT nextval('public.hashtags_id_seq'::regclass);
+
+
+--
+-- TOC entry 3015 (class 2604 OID 16576)
+-- Name: likes id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.likes ALTER COLUMN id SET DEFAULT nextval('public.likes_id_seq'::regclass);
+
+
+--
+-- TOC entry 3014 (class 2604 OID 16567)
+-- Name: posts id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.posts ALTER COLUMN id SET DEFAULT nextval('public.posts_id_seq'::regclass);
+
+
+--
+-- TOC entry 3012 (class 2604 OID 16473)
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- TOC entry 3030 (class 2606 OID 16585)
+-- Name: hashPost hashPost_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public."hashPost"
+    ADD CONSTRAINT "hashPost_pk" PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3022 (class 2606 OID 16499)
+-- Name: hashtags hashtags_hashtag_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.hashtags
+    ADD CONSTRAINT hashtags_hashtag_key UNIQUE (hashtag);
+
+
+--
+-- TOC entry 3024 (class 2606 OID 16497)
+-- Name: hashtags hashtags_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.hashtags
+    ADD CONSTRAINT hashtags_pk PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3028 (class 2606 OID 16578)
+-- Name: likes likes_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.likes
+    ADD CONSTRAINT likes_pk PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3026 (class 2606 OID 16571)
+-- Name: posts posts_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.posts
+    ADD CONSTRAINT posts_pk PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3018 (class 2606 OID 16479)
+-- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_email_key UNIQUE (email);
+
+
+--
+-- TOC entry 3020 (class 2606 OID 16477)
+-- Name: users users_pk; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pk PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3034 (class 2606 OID 16601)
+-- Name: hashPost hashPost_fk0; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public."hashPost"
+    ADD CONSTRAINT "hashPost_fk0" FOREIGN KEY ("hashtagId") REFERENCES public.hashtags(id);
+
+
+--
+-- TOC entry 3035 (class 2606 OID 16606)
+-- Name: hashPost hashPost_fk1; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public."hashPost"
+    ADD CONSTRAINT "hashPost_fk1" FOREIGN KEY ("postId") REFERENCES public.posts(id);
+
+
+--
+-- TOC entry 3032 (class 2606 OID 16591)
+-- Name: likes likes_fk0; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.likes
+    ADD CONSTRAINT likes_fk0 FOREIGN KEY ("userId") REFERENCES public.users(id);
+
+
+--
+-- TOC entry 3033 (class 2606 OID 16596)
+-- Name: likes likes_fk1; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.likes
+    ADD CONSTRAINT likes_fk1 FOREIGN KEY ("postId") REFERENCES public.posts(id);
+
+
+--
+-- TOC entry 3031 (class 2606 OID 16586)
+-- Name: posts posts_fk0; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.posts
+    ADD CONSTRAINT posts_fk0 FOREIGN KEY ("userId") REFERENCES public.users(id);
+
+
+-- Completed on 2023-08-18 18:55:59
+
+--
+-- PostgreSQL database dump complete
+--
+
