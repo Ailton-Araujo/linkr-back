32d709317347c6ce1344122c625d5f8666ed33fd
Ailton Araujo <ailton.correia.araujo@gmail.com>
2023-08-25 13:10:06 -0300

    fix:Comments

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -2,7 +2,6 @@ import { insertComment } from "../repositories/comments.repository.js";
 
 async function addComment(req, res) {
   const { postId, userId, comment } = req.body;
-  console.log(postId, userId, comment);
   try {
     await insertComment(postId, userId, comment);
     res.sendStatus(201);
